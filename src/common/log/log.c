0c1c82d625716645c03aab808cf6be8e980b3b1e
zonda yang <u226699@gmail.com>
2022-06-30 21:31:51 +0800

    Add logger function

--- src/common/log/log.c
+++ src/common/log/log.c
@@ -1,18 +1,40 @@
 
 #include "log.h"
 
+static int32_t log_level = LOG_NOTSET;
+
+#define uart_debug_log()                \
+    do {                                \
+        static char buf[1024];          \
+        va_list args;                   \
+                                        \
+        va_start(args, fmt);            \
+        ret = vsprintf(buf, fmt, args); \
+        va_end(args);                   \
+                                        \
+        printf("%s", buf);              \
+        uart_puts(uart, buf);           \
+    } while (0)
 
 int32_t debug_log(uart_inst_t *uart, const char *fmt, ...)
 {
-    static char buf[1024];
     int32_t ret;
-    va_list args;
+    uart_debug_log();
+    return ret;
+}
 
-    va_start(args, fmt);
-    ret = vsprintf(buf, fmt, args);
-    va_end(args);
+void set_level(int32_t level)
+{
+    log_level = level;
+}
 
-    printf("%s", buf);
-    uart_puts(uart, buf);
+int32_t logger(int32_t level, uart_inst_t *uart, const char *fmt, ...)
+{
+    if (level < log_level) {
+        return -1;
+    }
+
+    int32_t ret;
+    uart_debug_log();
     return ret;
 }
--- src/common/log/log.h
+++ src/common/log/log.h
@@ -1,9 +1,20 @@
 #ifndef COMMON_LOG_H
 #define COMMON_LOG_H
 
-#include "pico/stdlib.h"
 #include <stdarg.h>
+#include "pico/stdlib.h"
 
+enum {
+    LOG_NOTSET = 0,
+    LOG_DEBUG = 10,
+    LOG_INFO = 20,
+    LOG_WARNING = 30,
+    LOG_ERROR = 40,
+    LOG_CRITICAL = 50,
+};
+
+void set_level(int32_t level);
 int32_t debug_log(uart_inst_t *uart, const char *fmt, ...);
+int32_t logger(int32_t level, uart_inst_t *uart, const char *fmt, ...);
 
-#endif // COMMON_LOG_H
+#endif  // COMMON_LOG_H
