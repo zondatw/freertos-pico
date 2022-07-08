43ca80f3f62a13accf9f820fa9ac7f85794307ef
zonda yang <u226699@gmail.com>
2022-07-09 00:22:48 +0800

    Add mfrc522 header file

---
+++ src/feature/reader/mfrc522.h
@@ -0,0 +1,34 @@
+#ifndef MRFC522_H
+#define MRFC522_H
+
+
+// MI
+#define MI_OK 0x00
+#define MI_NOTAGERR 0x01
+#define MI_ERR 0x02
+
+// PCD
+#define PCD_TRANSCETIVE 0x0C
+#define PCD_RESETPHASE 0x0F
+
+// FIFO
+#define FIFO_DATA_REG 0x09
+#define FIFO_LEVEL_REG 0x0A
+
+// Control
+#define CONTROL_REG 0x0C
+#define COMMAND_REG 0x01
+
+// ERROR
+#define ERROR_REG 0x06
+
+#define T_MODE_REG 0x2A
+#define T_PRESCALER_REG 0x2B
+#define T_RELOAD_REG_L 0x2D
+#define T_RELOAD_REG_H 0x2C
+#define TX_AUTO_REG 0x15
+#define MODE_REG 0x11
+#define TX_CONTROL_REG 0x14
+
+
+#endif  // MRFC522_H
