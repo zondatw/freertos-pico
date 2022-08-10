c2b8314efdf3beafeb05b08e3e08ac39d3d42b4a
zonda yang <u226699@gmail.com>
2022-08-11 07:23:54 +0800

    Update monirot logic

--- src/feature/dummy/dummy.c
+++ src/feature/dummy/dummy.c
@@ -34,7 +34,6 @@ void get_task_state(void)
     unsigned portSHORT ulStatsAsPercentage;
     uint32_t ulTotalRunTime;
 
-    /* 获取任务总数目 */
     uxArraySize = uxTaskGetNumberOfTasks();
     if (uxArraySize > MAX_TASK_NUM) {
         printf("Too many task！\n");
@@ -46,34 +45,32 @@ void get_task_state(void)
     printf(
         "Task            status       ID       Pirority       stack        CPU\n");
 
-    /* 避免除零错误 */
-    if (ulTotalRunTime > 0) {
-        for (x = 0; x < uxArraySize; x++) {
-            char tmp[256];
+    uint8_t pcWriteBuffer[1024];
+    for (x = 0; x < uxArraySize; x++) {
+        char tmp[256];
 
-            ulStatsAsPercentage =
-                (uint16_t) (pxTaskStatusArray[x].ulRunTimeCounter) * 100 /
-                ulTotalRunTime;
+        ulStatsAsPercentage =
+            (uint16_t) (pxTaskStatusArray[x].ulRunTimeCounter) * 100 /
+            ulTotalRunTime;
 
-            if (ulStatsAsPercentage > 0UL) {
-                sprintf(tmp, "%-25s%-10c%-10lu%-12lu%-12d%d%%",
-                        pxTaskStatusArray[x].pcTaskName,
-                        task_state[pxTaskStatusArray[x].eCurrentState],
-                        pxTaskStatusArray[x].xTaskNumber,
-                        pxTaskStatusArray[x].uxCurrentPriority,
-                        pxTaskStatusArray[x].usStackHighWaterMark,
-                        ulStatsAsPercentage);
-            } else {
-                /* 任务运行时间不足总运行时间的1%*/
-                sprintf(tmp, "%-25s%-10c%-10lu%-12lu%-12dt<1%%",
-                        pxTaskStatusArray[x].pcTaskName,
-                        task_state[pxTaskStatusArray[x].eCurrentState],
-                        pxTaskStatusArray[x].xTaskNumber,
-                        pxTaskStatusArray[x].uxCurrentPriority,
-                        pxTaskStatusArray[x].usStackHighWaterMark);
-            }
-            printf("%s\n", tmp);
+
+        if (ulStatsAsPercentage > 0UL) {
+            sprintf(tmp, "%-25s%-10c%-10lu%-12lu%-12d%d%%",
+                    pxTaskStatusArray[x].pcTaskName,
+                    task_state[pxTaskStatusArray[x].eCurrentState],
+                    pxTaskStatusArray[x].xTaskNumber,
+                    pxTaskStatusArray[x].uxCurrentPriority,
+                    pxTaskStatusArray[x].usStackHighWaterMark,
+                    ulStatsAsPercentage);
+        } else {
+            sprintf(tmp, "%-25s%-10c%-10lu%-12lu%-12dt<1%%",
+                    pxTaskStatusArray[x].pcTaskName,
+                    task_state[pxTaskStatusArray[x].eCurrentState],
+                    pxTaskStatusArray[x].xTaskNumber,
+                    pxTaskStatusArray[x].uxCurrentPriority,
+                    pxTaskStatusArray[x].usStackHighWaterMark);
         }
+        printf("%s\n", tmp);
     }
     printf("Task state:   r-running  R-ready  B-block  S-suspend  D-delete\n");
 }
@@ -87,10 +84,10 @@ int main()
         .off_delay = 100,
     };
 
-    xTaskCreate(led_task, "LED Task 1", 256, &params, 1, NULL);
-    xTaskCreate(msg_task, "msg Task 1", 256, NULL, 1, NULL);
-    xTaskCreate(delay_task, "delay Task 1", 256, NULL, 1, NULL);
-    xTaskCreate(monitor_task, "monitor Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(led_task, "LED Task 1", 256, &params, 2, NULL);
+    xTaskCreate(msg_task, "msg Task 1", 256, NULL, 3, NULL);
+    xTaskCreate(delay_task, "delay Task 1", 256, NULL, 4, NULL);
+    xTaskCreate(monitor_task, "monitor Task 1", 1024, NULL, 1, NULL);
 
     vTaskStartScheduler();
     while (true)
